d404cb998496b105fa6250f36d71c33f1337f5b4
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-07-26 20:56:13 +0100

    Tradition

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Dots
+dots = document.getElementsByClassName("-dots");
+for (let i = 0; i < dots.length; i++) {
+  dots[i].innerHTML = `<span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <br>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <br>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>`;
+}
