d22d247c021bb862acee4793b78efa67204d8201
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-07-27 22:19:22 +0100

    Mobile prototype

--- js/main.js
+++ js/main.js
@@ -19,3 +19,11 @@ for (let i = 0; i < dots.length; i++) {
             <span></span>
             <span></span>`;
 }
+
+// Menu
+menu = document.getElementsByClassName("-menu")[0];
+dropdown = document.getElementsByClassName("-dropdown")[0];
+menu.onclick = () => {
+  dropdown.classList.toggle("-active");
+  document.body.classList.toggle("-no-scroll");
+};
