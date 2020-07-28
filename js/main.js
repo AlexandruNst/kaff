3e09c7dff6f91a865f546d2953e5de46b38cd5cd
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-07-28 19:15:02 +0100

    Fixed sizing on mobile

--- js/main.js
+++ js/main.js
@@ -23,7 +23,9 @@ for (let i = 0; i < dots.length; i++) {
 // Menu
 menu = document.getElementsByClassName("-menu")[0];
 dropdown = document.getElementsByClassName("-dropdown")[0];
+hamburger = document.getElementsByClassName("fa-stream")[0];
 menu.onclick = () => {
   dropdown.classList.toggle("-active");
   document.body.classList.toggle("-no-scroll");
+  hamburger.classList.toggle("fa-times");
 };
